6d8c30a106dba28212abece64597483d5daa3b42
Ryan Hanks <ryandhanks@gmail.com>
2021-11-23 11:45:46 -0600

    wip

--- folder.tf
+++ folder.tf
@@ -5,7 +5,7 @@ resource "google_folder" "group_folder" {
 
 resource "google_folder_iam_member" "default_service_account_environment_group_folder" {
   folder   = google_folder.group_folder.folder_id
-  member   = "serviceAccount:${var.service_account_email}"
+  member   = "serviceAccount:${module.project.service_account_email}"
   for_each = toset([
     "roles/resourcemanager.folderAdmin", "roles/resourcemanager.organizationAdmin", "roles/owner",
     "roles/resourcemanager.projectCreator", "roles/resourcemanager.projectIamAdmin", "roles/billing.projectManager",
