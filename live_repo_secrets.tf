a86804fcc021c9d15a1f5b36557107f6d97e6213
Ryan Hanks <ryandhanks@gmail.com>
2021-11-24 01:34:24 -0600

    wip

--- live_repo_secrets.tf
+++ live_repo_secrets.tf
@@ -5,7 +5,7 @@ locals {
     "billing_account" : var.billing_account,
 #    "service_account_id" : module.project.service_account_id,
     "service_account_email" : module.project.service_account_email,
-    "environment_group_name" : var.name,
+    "group_name" : var.name,
     "group_folder_id" : google_folder.group_folder.folder_id
   })
 }
