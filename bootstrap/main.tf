4ec31bc6f435d616e8e8ec8c7f2f561f1f121d1b
Ryan Hanks <ryandhanks@gmail.com>
2021-11-24 01:23:01 -0600

    wip

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -4,21 +4,15 @@ output "billing_account" {
   value = var.billing_account
 }
 
-variable "environment_group_name" {}
+variable "group_name" {}
 output "environment_group_name" {
-  value = var.environment_group_name
+  value = var.group_name
 }
 
 variable "organization_id" {}
 output "organization_id" {
   value = var.organization_id
 }
-
-
-#variable "service_account_id" {}
-#output "service_account_id" {
-#  value = var.service_account_id
-#}
 
 variable "service_account_email" {}
 output "service_account_email" {
