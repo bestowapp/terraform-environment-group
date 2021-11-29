b2088d058e91558e7e27e7650cac39983b399086
Ryan Hanks <ryandhanks@gmail.com>
2021-11-29 15:14:51 -0600

    wip

--- variables.tf
+++ variables.tf
@@ -2,14 +2,7 @@ variable "billing_account" {}
 
 variable "display_name" {}
 
+variable "name" {}
+
 variable "organization_id" {}
 
-#variable "bucket_name" {}
-
-#variable "service_account_id" {}
-#
-#variable "service_account_email" {}
-
-#variable "project_id" {}
-
-variable "name" {}
