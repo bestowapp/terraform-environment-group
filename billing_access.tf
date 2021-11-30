98d762362963a17e8fb3f2edc1e4470099960565
Ryan Hanks <ryandhanks@gmail.com>
2021-11-30 17:34:54 -0600

    Add admin role bindings

--- billing_access.tf
+++ billing_access.tf
@@ -1,6 +1,6 @@
-resource "google_organization_iam_member" "billing_access" {
+resource "google_organization_iam_member" "admin_bindings" {
   member   = "serviceAccount:${module.project.service_account_email}"
   org_id = var.organization_id
-  for_each = toset(["roles/billing.projectManager", "roles/billing.user", "roles/resourcemanager.projectCreator"])
+  for_each = toset(["roles/billing.projectManager", "roles/billing.user", "roles/resourcemanager.projectCreator", "roles/iam.securityAdmin", "roles/resourcemanager.folderAdmin"])
   role   = each.value
 }
